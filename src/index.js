bd681bb5956be269db8dff448419740b46795574
Lisandro <lisansierra@outlook.com>
2025-04-11 10:01:25 -0400

    finish env

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-import express from "express";
-import employeesRoutes from './routes/employees.routes.js';
-import indexRoutes from './routes/index.routes.js'
+import app from './app.js';
+import { PORT } from './config.js';
 
-
-const app = express();
-
-
-app.use(express.json());
-app.use('/api',employeesRoutes);
-app.use(indexRoutes);
-
-
-
-
-const port = 4000;
-
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+app.listen(PORT);
+console.log(`Server on port http://localhost:${PORT}`);
